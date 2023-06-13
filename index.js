ba66c1aa7c73afec6262be472da1caf3d553c0ce
Kenneth Childs Jr <103333336+kchilds1@users.noreply.github.com>
2023-06-13 13:03:27 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,14 @@
 let expense = document.getElementById("expense");
 console.log(expense);
 
+//create an array for all the amount values
+let amountSpent = [];
+
 //PULL DATE FROM HTML INTO JAVASCRIPT
 let purchaseDate = document.getElementById("Date");
 
 //PULL AMOUNT FROM HTML INTO JAVASCRIPT
-let amountSpent = document.getElementById("amount-spent");
+let amountSpentInput = document.getElementById("amount-spent");
 
 //PULL BUTTON FROM HTML INTO JAVASCRIPT
 let addExpense = document.getElementById("add-expense");
@@ -23,20 +26,42 @@ function eButton(){
       <tr>
         <td>${expense.value}</td>
         <td>${purchaseDate.value}</td>
-        <td>$${amountSpent.value}</td>
+        <td>$${amountSpentInput.value}</td>
         <td><button class = "deleteBtn">DELETE</button></td>
       </tr>
     `;
-    
+    totalAmount();
  }
-
+ 
  function onDeleteRow(x){
     if(!x.target.classList.contains("deleteBtn")){//target is what was clicked on
+        
         return;
     }
     
     const btn = x.target;//making it known that target is the button
     btn.closest("tr").remove(); //THIS WILL REMOVE THE CLOSEST TABLE ROW WHEN I CLICK THE DELETE BUTTON
+    
+    // Update the total amount after deleting an expense
+    totalAmount();
  }
 
- //let totalExpense = 
+  
+function totalAmount() {
+  // Reset the amountSpent array
+  amountSpent = [];
+  
+  // Loop through each expense row and add the amount to the array
+  const expenseRows = document.querySelectorAll("#tbody-element tr");
+  expenseRows.forEach(row => {
+    const amountCell = row.querySelector("td:nth-child(3)");
+    amountSpent.push(parseFloat(amountCell.textContent.substring(1))); // Extract the numeric value
+  });
+  
+  // Sum up the values in the amountSpent array
+  let totalAmountSpent = amountSpent.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+  console.log(totalAmountSpent);
+  
+  const sum = document.getElementById("sum");
+  sum.innerHTML = `<h3>The total amount of expenses is: $${totalAmountSpent}</h3>`;
+}
